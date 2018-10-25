1d0a2e7df602ced3a24a2c86c7e0ba89e3256faa
joel <whocares>
2018-10-25 19:36:43 +1000

    Skip every second frame

--- play.c
+++ play.c
@@ -278,6 +278,12 @@ void playLogic(RootState* state, const byte playerNumber) {
     }
 
     if (emulatorState->emu_state->lcd_entered_vblank) {
+        state->Players[playerNumber].WasFrameSkipped = !state->Players[playerNumber].WasFrameSkipped;
+        if (state->Players[playerNumber].WasFrameSkipped) {
+            frameCount++;
+            return;
+        }
+
         GbController* input = calloc(1, sizeof(GbController));
 
         bool pressedButtons[N64_BUTTON_COUNT] = {};
@@ -329,6 +335,10 @@ void playLogic(RootState* state, const byte playerNumber) {
  * @param playerNumber player in play mode.
  */
 void playDraw(const RootState* state, const byte playerNumber) {
+    if (state->Players[playerNumber].WasFrameSkipped) {
+        return;
+    }
+
     // Main background.
     Rectangle screen = {};
     getScreenPosition(state, playerNumber, &screen);
--- state.h
+++ state.h
@@ -23,6 +23,7 @@ typedef struct {
     N64Button GbStartButton;
     N64Button GbSelectButton;
     CartridgeData Cartridge;
+    bool WasFrameSkipped;
     bool AudioEnabled;
     GbState EmulationState;
     SuperGameboyState SGBState;
