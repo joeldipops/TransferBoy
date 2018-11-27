59bc3405caf1c7e8234f986382bc5069a059db7c
joeldipops <jolacus@hotmail.com>
2018-11-27 11:55:39 +1000

    Finished 8 bit loads.

--- asm/test.asm
+++ asm/test.asm
@@ -17,6 +17,8 @@ SECTION "start",HOME[$0100]
     ROM_HEADER ROM_NOMBC, ROM_SIZE_32KBYTE, RAM_SIZE_0KBYTE
 
 begin:
+    ; EIGHT BIT LOADS
+
     ld A, 1
     ld B, 2
     ld C, 3
@@ -172,11 +174,64 @@ begin:
     ; A=5 B=$c0 C=0 D=$c2 E=$b2 H=$c0 L=$10
 
     ld A, 9
-    ld [$c33c], A
+    ; GameBoy CPU Manual says this is a "Two Byte immediate value, LS byte first", but I'm pretty sure it's LS byte second...
+    ld [$c33c], A    
     ld A, 0
     ld A, [$c33c]
     ; A=9 B=2 C=3 D=4 E=5 H=6 L=7
 
+    ; ld A, [C] is an alias to ld A, [$FF00+C].  Use both to check the right memory location is being written to.
+    ld C, $12
+    ld A, 13
+    ld [C], A
 
+    ld A, [$ff00+C]
+    ld B, A
 
+    ld A, 0
+    ld A, [C]
+    ; A=13 B=$0d C=$12 D=4 E=5 H=6 L=7
 
+    ld H, $c2
+    ld L, $b2
+    
+    ld A, 0
+    ld [$c2b0], A
+    ld A, 2
+    ld [$c2b1], A
+    ld A, 1
+    ld [$c2b2], A
+    ; $c2b0 = $00 $02 $01
+    
+    ; LOAD and DECREMENT/INCREMENT
+    
+    ldd A, [HL]
+    ; A=1 B=$0d C=$12 D=4 E=5 H=$c2 L=$b1
+    
+    ldd [HL], A
+    ld A, 0
+    ld A, [$c2b1]
+    ; A=1 B=$0d C=$12 D=4 E=5 H=$c2 L=$b0
+    
+    ldi A, [HL]
+    ; A=0 B=$0d C=$12 D=4 E=5 H=$c2 L=$b1
+    
+    ld A, 3
+    ldi [HL], A
+    ld A, 0
+    ld A, [$c2b1]
+    ; A=3 B=$0d C=$12 D=4 E=5 H=$c2 L=$b2   
+    
+    ; LOAD $ff00 + n
+    ldh [3], A
+    ld A, 0
+    ldh A, [3]
+    ; A=3 B=$0d C=$12 D=4 E=5 H=$c2 L=$b2
+    
+    ld A, 0
+    ld A, [$ff03]
+    ; A=3 B=$0d C=$12 D=4 E=5 H=$c2 L=$b2    
+
+    ; SIXTEEN BIT LOADS
+    
+    
