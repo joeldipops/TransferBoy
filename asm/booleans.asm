35a6da8d24209efd3766454d4ba8ca5c19bd802a
joeldipops <joeldipops@gmail.com>
2018-12-13 21:05:27 +1000

    ORs added

--- asm/booleans.asm
+++ asm/booleans.asm
@@ -1,3 +1,5 @@
+ ;  AND
+
     ld A, %10101010 ; 170/ $AA
     ld B, 1
     ld C, 2
@@ -102,3 +104,164 @@
     ld A, $0f
     and A, $20
     ; AF=00xx BC=0102 DE=2020 HL=cabe ZNHC=1010     
+
+;   OR
+    ld A, %10100101 ; $a5
+    or A
+    ; AF=a500 BC=0102 DE=2020 HL=cabe ZNHC=0000
+
+    ld A, 0
+    or A
+    ; AF=0001 BC=0102 DE=2020 HL=cabe ZNHC=1000
+
+; or B
+    ld A, %10101010
+    ld B, %01010101
+    or B
+    ; AF=ff00 BC=5502 DE=2020 HL=cabe ZNHC=0000
+
+    ld B, $0f
+    sub A
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld B, 0
+    or B
+    ; AF=0001 BC=0002 DE=2020 HL=cabe ZNHC=1000
+
+    ld A, 1
+    or B
+    ; AF=0100 BC=0002 DE=2020 HL=cabe ZNHC=0000
+
+; or C
+    ld A, %10101010
+    ld C, %01010101
+    or C
+    ; AF=ff00 BC=0055 DE=2020 HL=cabe ZNHC=0000
+
+    ld B, $0f
+    sub B
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld C, 0
+    or C
+    ; AF=0001 BC=0f00 DE=2020 HL=cabe ZNHC=1000
+
+    ld A, 0
+    ld C, 1
+    or C
+    ; AF=0100 BC=0f01 DE=2020 HL=cabe ZNHC=0000
+
+; or D
+    ld A, %10101010
+    ld D, %01010101
+    or D
+    ; AF=ff00 BC=0f01 DE=5520 HL=cabe ZNHC=0000
+
+    sub B
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld D, 0
+    or D
+    ; AF=0001 BC=ff01 DE=0020 HL=cabe ZNHC=1000
+
+    ld D, 1
+    ld A, 0
+    or D
+    ; AF=0100 BC=0f01 DE=0120 HL=cabe ZNHC=0000
+
+; or E
+    ld A, %10101010
+    ld E, %01010101
+    or E
+    ; AF=ff00 BC=0f01 DE=0155 HL=cabe ZNHC=0000
+
+    sub B
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld E, 0
+    or E
+    ; AF=0001 BC=0f01 DE=0100 HL=cabe ZNHC=1000
+
+    ld A, 1
+    or E
+    ; AF=0100 BC=0f01 DE=0100 HL=cabe ZNHC=0000
+
+; or H
+    ld A, %10101010
+    ld H, %01010101
+    or H
+    ; AF=ff00 BC=0f01 DE=0100 HL=55be ZNHC=0000
+
+    ld B, $0f
+    sub B
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld H, 0
+    or H
+    ; AF=0001 BC=0f01 DE=0100 HL=00be ZNHC=1000
+
+    ld A, 0
+    ld H, $23
+    or H
+    ; AF=2300 BC=0f01 DE=0100 HL=23be ZNHC=0000
+
+; or L
+    ld A, %10101010
+    ld L, %01010101
+    or L
+    ; AF=ff00 BC=0f01 DE=0100 HL=2355 ZNHC=0000
+
+    ld B, $0f
+    sub B
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld L, 0
+    or L
+    ; AF=0001 BC=0f01 DE=0100 HL=2355 ZNHC=1000
+
+    ld A, 0
+    ld L, $da
+    or L
+    ; AF=da00 BC=0f01 DE=0100 HL=23da ZNHC=0000
+
+    ld H, $cb
+    ld L, $de
+
+    ld A, %10101010
+    ld [HL], %01010101
+    or [HL]
+    ; AF=ff00 BC=0f01 DE=0100 HL=cbde ZNHC=0000
+
+    ld B, $0f
+    sub B
+    ; ^ set some flags so we know they get reset
+    ld A, 0
+    ld [HL], 0
+    or [HL]
+    ; AF=0001 BC=0f01 DE=0100 HL=cbde ZNHC=1000
+
+    ld A, 0
+    ld [HL], $f3
+    or [HL]
+    ; AF=f300 BC=0f01 DE=0100 HL=cbde ZNHC=0000
+
+    ld A, 1
+    or [HL]
+    ; AF=f300 BC=0f01 DE=0100 HL=cbde ZNHC=0000
+
+    ld [HL], 4
+    or [HL]
+    ; AF=f700 BC=0f01 DE=0100 HL=cbde ZNHC=0000
+
+    ld A, 1
+    sub B
+    ; ^ some flags set
+    ld A,   %00101110
+    or      %01101101
+    ; AF=6400 BC=0f01 DE=0100 HL=cbde ZNHC=0000
+
+    ld A, 0
+    or 0
+    ; AF=0001 BC=0f01 DE=0100 HL=cbde ZNHC=0000
+
+; XORs
