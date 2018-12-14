e9edae9635f3044f828680f2bfbd7398382a96c1
joeldipops <joeldipops@gmail.com>
2018-12-14 23:21:44 +1000

    fixed mksprite.sh

--- mksprite.sh
+++ mksprite.sh
@@ -1,3 +1,3 @@
-../libdragon/tools/mksprite/mksprite 16 14 7 ./assets/textMap.png ./filesystem/textMap.sprite
-../libdragon/tools/mksprite/mksprite 16 8 2 ./assets/spriteSheet.png ./filesystem/spriteSheet.sprite
+mksprite 16 14 7 ./assets/textMap.png ./filesystem/textMap.sprite
+mksprite 16 8 2 ./assets/spriteSheet.png ./filesystem/spriteSheet.sprite
 
