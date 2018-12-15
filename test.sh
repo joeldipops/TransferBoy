b91e0e5b1cee17d1d3429a2bf264f08cfca10338
joeldipops <joeldipops@gmail.com>
2018-12-15 16:15:51 +1000

    test.sh changes

--- test.sh
+++ test.sh
@@ -1,7 +1,7 @@
 # make gb roms
-cd ./asm
-./make.sh
-cd ../
+#cd ./asm
+#./make.sh
+#cd ../
 
 ./mksprite.sh
 
@@ -9,12 +9,13 @@ cd ../
 make clean
 make all
 
+cp transferboy64.z64 /media/sf_VMSharedFolder/
 
 # Op codes test ROM
-cen64 -multithread -controller num=1,tpak_rom=/home/joel/TransferBoy/filesystem/opTest.gb -eep4k ~/TransferBoy/log.eeprom ~/n64devutils/cen64/data/pifdata.bin  ~/TransferBoy/transferboy64.z64
+#cen64 -multithread -controller num=1,tpak_rom=/home/joel/TransferBoy/filesystem/opTest.gb -eep4k ~/TransferBoy/log.eeprom ~/n64devutils/cen64/data/pifdata.bin  ~/TransferBoy/transferboy64.z64
 
 # utils routines test rom
 #cen64 -multithread -controller num=1,tpak_rom=~/TransferBoy/filesystem/routineTest.gb -eep4k ~/TransferBoy/log.eeprom ~/n64devutils/cen64/data/pifdata.bin  ~/TransferBoy/transferboy64.z64
 
 # 2-Player Test ROM
-#cen64 -multithread -controller num=1,tpak_rom=/home/joel/n64devutils/gbC-test/2player.gb -eep4k ~/TransferBoy/log.eeprom ~/n64devutils/cen64/data/pifdata.bin  ~/TransferBoy/transferboy64.z64
+cen64 -multithread -controller num=1,tpak_rom=/home/joel/n64devutils/gbC-test/2player.gb -eep4k ~/TransferBoy/log.eeprom ~/n64devutils/cen64/data/pifdata.bin  ~/TransferBoy/transferboy64.z64
