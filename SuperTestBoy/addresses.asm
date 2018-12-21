35526589debc2ece1526aadc333a1fb902e15676
joeldipops <jolacus@hotmail.com>
2018-12-21 21:40:14 +1000

    more init stuff

--- SuperTestBoy/addresses.asm
+++ SuperTestBoy/addresses.asm
@@ -1,16 +1,25 @@
     IF !DEF(ADDRESSES_INCLUDED)
 ADDRESSES_INCLUDED SET 1
 
-;;;;;;;;;;;;;;; ADDRESSES ;;;;;;;;;;;;;;;;;;;;;
+;;;;;;;;;;;;;;; Hardware Addresses ;;;;;;;;;;;;;;;;;;;;;
+BackgroundMap1 EQU $9800
+BackgroundMap2 EQU $9c00
+
+JoypadIo EQU $ff00
 InterruptFlags EQU $ff0f
 
-DMASourceRegister EQU $ff46
-runDMA EQU $ff80
+LcdControl EQU $ff40
 
-VBlankFlag EQU $01
+BackgroundScrollY EQU $ff42
+BackgroundScrollX EQU $ff43
 
-JoypadRegister EQU $ff00
-GetButtonsBit EQU %0010000
-GetDPadBit EQU %00010000
-ClearJoypad EQU %00110000
+DmaSourceRegister EQU $ff46
+BackgroundPalette EQU $ff47
+SpritePalette1 EQU $ff48
+SpritePalette2 EQU $ff49
+
+;;;;;;;;;;;;;; Software Addresses ;;;;;;;;;;;;;;;;;;;;;;
+stackFloor EQU $ffff ; Might change to DFFF when actually start using the stack
+runDma EQU $ff80
+
     ENDC
