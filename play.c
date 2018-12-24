5897f86d986037263ef65f29065d7bde92a12347
joeldipops <joeldipops@gmail.com>
2018-12-24 10:39:57 +1000

    Mode switch clean up

--- play.c
+++ play.c
@@ -243,10 +243,8 @@ void renderPixels(
 
     long long thisClock = get_ticks_ms();
     long diff = thisClock - lastClock;
-    string mode = "";
 
-    strcpy(mode,  treeMode ? "Tree" : "Default");
-    sprintf(text, "Frames: %lld FPS: %f Mode: %s", frameCount, (2.0 / (double)diff) * 1000, mode);
+    sprintf(text, "Frames: %lld FPS: %f", frameCount, (2.0 / (double)diff) * 1000);
     lastClock = thisClock;
     graphics_set_color(GLOBAL_TEXT_COLOUR, 0x0);
     graphics_draw_box(frame, 0, 0, 680, 10, GLOBAL_BACKGROUND_COLOUR);
@@ -331,10 +329,6 @@ void playLogic(RootState* state, const byte playerNumber) {
 
         bool releasedButtons[N64_BUTTON_COUNT] = {};
         getPressedButtons(&state->KeysReleased, playerNumber, releasedButtons);
-
-        if (state->KeysReleased.c[playerNumber].C_down) {
-            treeMode = !treeMode;
-        }
 
         if (releasedButtons[state->Players[playerNumber].SystemMenuButton]) {
             state->Players[playerNumber].ActiveMode = Menu;
