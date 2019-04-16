144b103dc7935f436f3e140d66d1412e81e272c0
joeldipops <joeldipops@gmail.com>
2019-04-16 21:52:17 +1000

    Started work on import loops

--- include/tpakio.c
+++ include/tpakio.c
@@ -11,8 +11,8 @@
  * command 02-CXXX to FXXX-GB read ROM in 32kbyte chunks - XXX bank-offset
  ******************/
 
-// For some
 const byte BLOCK_SIZE = 32;
+const natural BANK_SIZE = 16 * 1024; // 16kB banks.
 
 // According to the cen64 source, address is 0x8000 and the 1 is from a 5 bit cyclic redundancy check
 const natural ENABLE_TPAK_ADDRESS = 0x8001; 
@@ -39,84 +39,6 @@ const natural TPAK_BANK_SWITCH_ADDRESS = 0xA00C;
 
 const natural ROM_ADDRESS_OFFSET = 0xC000;
 
-// Must be packed into a single byte to fit in the header.
-typedef enum __attribute__ ((packed)) {
-    GBC_NOT_SUPPORTED = 0x00, 
-    GBC_DMG_SUPPORTED = 0x80,
-    GBC_ONLY_SUPPORTED = 0xC0 
-} GbcSupport;
-
-// Must be packed into a single byte to fit in the header
-typedef enum __attribute__ ((packed)) {
-    // 32kB ROM
-    ROM_ONLY = 0x00,
-
-    // MBC1 - max 2MB ROM and/or 32kB RAM)
-    MBC1 = 0x01,
-    MBC1_RAM = 0x02, 
-    MBC1_BATTERY = 0x03,
-    
-    // MBC2 - max 256kB ROM and 256B RAM
-    MBC2 = 0x05,
-    MBC2_BATTERY = 0x06,
-    MBC2_RAM = 0x08,
-    MBC2_RAM_BATTERY = 0x09,
-
-    // MMO1 - max 8MB ROM and 128kB RAM
-    MMM01 = 0x0B,
-    MMM01_RAM = 0x0C,
-    MMM01_RAM_BATTERY = 0x0D,
-
-    // MBC3 - max 2MB ROM and/or 32kB RAM and Timer
-    MBC3_TIMER_BATTERY = 0x0f,
-    MBC3_TIMER_RAM_BATTERY = 0x10,
-    MBC3 = 0x11,
-    MBC3_RAM = 0x012,
-    MBC3_RAM_BATTERY = 0x013,
-
-    // MBC5 - max 8MB ROM and/or 128kB RAM
-    MBC5 = 0x19,
-    MBC5_RAM = 0x1A,
-    MBC5_RAM_BATTERY = 0x1B,
-    MBC5_RUMBLE = 0x1C,
-    MBC5_RUMBLE_RAM = 0x1D,
-    MBC5_RUMBLE_RAM_BATTERY = 0x1E,
-
-    // MBC6 - who knows?
-    MBC6 = 0x20,
-
-    // MBC7 - max 8MB ROM or 256kB RAM and Accelerometer
-    MBC7_SENSOR_RUMBLE_RAM_BATTERY = 0x22    
-} CartridgeType;
-
-typedef struct {
-    byte entryScript[4];
-    byte logo[48];
-    union {
-        byte DMGTitle[16];
-        struct {
-            byte DMGTitle[15];
-            GbcSupport GbcSupport;
-        } OldCGBTitle;
-        struct {
-            byte CGBTitle[11];
-            byte ManufacturerCode[4];
-            GbcSupport GbcSupport;            
-        } CGBTitle;
-    };
-    natural NewLicenseeCode;
-    bool IsSgbSupported;
-    CartridgeType CartridgeType;
-    byte RomSizeCode;
-    byte RamSizeCode;
-    byte Destination;
-    byte OldLicenseeCode;
-    byte VersionNumber;
-    byte HeaderChecksum;
-    natural GlobalChecksum;
-    byte overflow[16];
-} CartridgeHeader;
-
 /**
  * Puts Tpak/Cartridge in to a state where it's ready to be read from.
  * @param controllerNumber controller slot the Tpak is plugged in to.
@@ -135,7 +57,7 @@ sByte initialiseTPak(const u8 controllerNumber) {
         return -2;
     }
 
-    byte block[32];
+    byte block[BLOCK_SIZE];
     sByte result = 0;    
 
     // Wake up the transfer pak
@@ -294,13 +216,13 @@ natural getRamBankSize(CartridgeHeader* header) {
  **  -1 - Invalid controller slot (must be 1-4)
  **  -2 - Cartridge header contains invalid values.
  **  -3 - Header failed checksum
- ** -10 - Controller not plugged in.
- ** -20 - Transfer pak not detected.
- ** -30 - Data corruption.
- ** -40 - No cartridge in Tpak.
- ** -50 - Tpak not behaving as expected.
+ ** -11 - Controller not plugged in.
+ ** -12 - Transfer pak not detected.
+ ** -13 - Data corruption.
+ ** -14 - No cartridge in Tpak.
+ ** -15 - Tpak not behaving as expected.
  */
-sByte importCartridge(byte controllerNumber, CartridgeData* cartridge) {
+sByte importCartridge(byte controllerNumber, GameBoyCartridge* cartridge) {
     if (controllerNumber <= 0 || controllerNumber > 4) {
         return -1;
     }
@@ -309,12 +231,13 @@ sByte importCartridge(byte controllerNumber, CartridgeData* cartridge) {
 
     sByte result = initialiseTPak(controllerNumber);
     if (result) {
-        return result * 10;
+        return result + 10;
     }
 
     CartridgeHeader header;
-    if (getHeader(controllerNumber, &header)) {
-        return result * 10;
+    result = getHeader(controllerNumber, &header);
+    if (result) {
+        return result + 10;
     }
 
     if (!checkHeader(&header)) {
@@ -328,6 +251,17 @@ sByte importCartridge(byte controllerNumber, CartridgeData* cartridge) {
     if (romBanks < 0 || ramBanks < 0 || ramBankSize < 0) {
         return -2; 
     }
+
+    result = _importRom(controllerNumber, cartridge);
+    if (result) {
+        return result + 20;
+    }
+
+    result = _importRam(controllerNumber, cartridge);
+    if (result) {
+        return result + 30;
+    }
+
     return 0;
 }
 
@@ -335,24 +269,36 @@ sByte importCartridge(byte controllerNumber, CartridgeData* cartridge) {
 /**
  * Gets the complete ROM data from the cartridge a transfer pak.
  * @param controllerNumber T-Pak plugged in to this controller slot.
- * @param romData ROM will be copied to this address.
+ * @param cartridge Structure to copy ROM to.
  * @returns Error codes
  ** 0  - Successful
  ** -1 - Error
  */
-sByte getRom(const byte controllerNumber, ByteArray* romData) {
+sByte _importRom(const byte controllerNumber, GameBoyCartridge* cartridge) {
+    // Loop through each bank
+
+    cartridge->Rom.Data = calloc(cartridge->RomBankCount * BANK_SIZE, 1);
+    byte* pointer = (byte*) cartridge->Rom.Data;
+
+    for (natural bank = 0; bank < cartridge->RomBankCount; bank++) {
+        // Read into memory 32bytes at a time.
+        for (natural address = 0; address < BANK_SIZE; address += BLOCK_SIZE) {
+            read_mempak_address(controllerNumber, ROM_ADDRESS_OFFSET + address, pointer + (address * bank));
+        }
+    }
+    
     return -1;
 }
 
 /**
  * Gets the complete ROM data from the cartridge in a transfer pak.
  * @param controllerNumber T-Pak plugged in to this controller slot.
- * @param ramData RAM will be copied to this address.
+ * @param cartridge Structure to copy RAM to.
  * @returns Error codes
  **  0 - Successful
  ** -1 - Error
  */
-sByte getRam(const byte controllerNumber, ByteArray* ramData) {
+sByte _importRam(const byte controllerNumber, GameBoyCartridge* cartridge) {
     return -1;
 }
 
--- include/tpakio.h
+++ include/tpakio.h
@@ -1,7 +1,94 @@
-#ifndef TPAKIO_INLCUDED
+#ifndef TPAKIO_INCLUDED
 #define TPAKIO_INCLUDED
 
 #include "core.h"
+
+// Must be packed into a single byte to fit in the header.
+typedef enum __attribute__ ((packed)) {
+    GBC_NOT_SUPPORTED = 0x00, 
+    GBC_DMG_SUPPORTED = 0x80,
+    GBC_ONLY_SUPPORTED = 0xC0 
+} GbcSupport;
+
+// Must be packed into a single byte to fit in the header
+typedef enum __attribute__ ((packed)) {
+    // 32kB ROM
+    ROM_ONLY = 0x00,
+
+    // MBC1 - max 2MB ROM and/or 32kB RAM)
+    MBC1 = 0x01,
+    MBC1_RAM = 0x02, 
+    MBC1_BATTERY = 0x03,
+    
+    // MBC2 - max 256kB ROM and 256B RAM
+    MBC2 = 0x05,
+    MBC2_BATTERY = 0x06,
+    MBC2_RAM = 0x08,
+    MBC2_RAM_BATTERY = 0x09,
+
+    // MMO1 - max 8MB ROM and 128kB RAM
+    MMM01 = 0x0B,
+    MMM01_RAM = 0x0C,
+    MMM01_RAM_BATTERY = 0x0D,
+
+    // MBC3 - max 2MB ROM and/or 32kB RAM and Timer
+    MBC3_TIMER_BATTERY = 0x0f,
+    MBC3_TIMER_RAM_BATTERY = 0x10,
+    MBC3 = 0x11,
+    MBC3_RAM = 0x012,
+    MBC3_RAM_BATTERY = 0x013,
+
+    // MBC5 - max 8MB ROM and/or 128kB RAM
+    MBC5 = 0x19,
+    MBC5_RAM = 0x1A,
+    MBC5_RAM_BATTERY = 0x1B,
+    MBC5_RUMBLE = 0x1C,
+    MBC5_RUMBLE_RAM = 0x1D,
+    MBC5_RUMBLE_RAM_BATTERY = 0x1E,
+
+    // MBC6 - who knows?
+    MBC6 = 0x20,
+
+    // MBC7 - max 8MB ROM or 256kB RAM and Accelerometer
+    MBC7_SENSOR_RUMBLE_RAM_BATTERY = 0x22    
+} CartridgeType;
+
+typedef struct {
+    byte entryScript[4];
+    byte logo[48];
+    union {
+        byte DMGTitle[16];
+        struct {
+            byte DMGTitle[15];
+            GbcSupport GbcSupport;
+        } OldCGBTitle;
+        struct {
+            byte CGBTitle[11];
+            byte ManufacturerCode[4];
+            GbcSupport GbcSupport;            
+        } CGBTitle;
+    };
+    natural NewLicenseeCode;
+    bool IsSgbSupported;
+    CartridgeType CartridgeType;
+    byte RomSizeCode;
+    byte RamSizeCode;
+    byte Destination;
+    byte OldLicenseeCode;
+    byte VersionNumber;
+    byte HeaderChecksum;
+    natural GlobalChecksum;
+    byte overflow[16];
+} CartridgeHeader;
+
+typedef struct {
+    ByteArray Rom;
+    ByteArray Ram;
+    CartridgeHeader Header;
+    natural RomBankCount;
+    byte RamBankCount;
+    natural RamBankSize;
+} GameBoyCartridge;
 
 /**
  * Imports the entire cartridge in to RAM as CartridgeData
@@ -18,37 +105,27 @@
  ** -40 - No cartridge in Tpak.
  ** -50 - Tpak not behaving as expected.
  */
-sByte importCartridge(const byte controllerNumber, CartridgeData* cartridge);
+sByte importCartridge(byte controllerNumber, GameBoyCartridge* cartridge);
 
 /**
  * Gets the complete ROM data from the cartridge a transfer pak.
  * @param controllerNumber T-Pak plugged in to this controller slot.
- * @out romData ROM will be copied to this address.
+ * @param cartridge Structure to copy ROM to.
  * @returns Error codes
  ** 0  - Successful
  ** -1 - Error
  */
-sByte getRom(const byte controllerNumber, ByteArray* romData);
+sByte _importRom(const byte controllerNumber, GameBoyCartridge* cartridge);
 
 /**
  * Gets the complete ROM data from the cartridge in a transfer pak.
  * @param controllerNumber T-Pak plugged in to this controller slot.
- * @out ramData RAM will be copied to this address.
+ * @param cartridge Structure to copy RAM to.
  * @returns Error codes
  **  0 - Successful
  ** -1 - Error
  */
-sByte getRam(const byte controllerNumber, ByteArray* ramData);
-
-/**
- * Sets the cartridge RAM with the data in ramData.
- * @param controllerNumber T-Pak plugged in to this controller slot.
- * @param ramData RAM to copy in to the cartridge.
- * @returns Error codes
- **  0 - Successful
- ** -1 - Error
- */
-sByte setRam(const u8 controllerNumber, ByteArray* ramData);
+sByte _importRam(const byte controllerNumber, GameBoyCartridge* cartridge);
 
 
 /*********************************************************************************************
