0c68cf0ab21e5139a22c2f73b2d8324eb85a1548
joeldipops <joeldipops@gmail.com>
2019-05-26 15:42:09 +1000

    Another test rom line added

--- test.sh
+++ test.sh
@@ -26,7 +26,7 @@ make all
 #~/Projects/tools/cen64/cen64 -multithread -controller num=1,tpak_rom=/home/joeldipops/Projects/tools/cen64/mbc3.gb,tpak_save=/home/joeldipops/Projects/tools/cen64/mbc3.sav ~/Projects/tools/cen64/pifdata.bin  ~/Projects/TransferBoy/transferboy64.z64
 
 # mbc5 Test ROM
-~/Projects/tools/cen64/cen64 -multithread -controller num=1,tpak_rom=/home/joeldipops/Projects/tools/cen64/mbc5.gbc,tpak_save=/home/joeldipops/Projects/tools/cen64/mbc5.sav ~/Projects/tools/cen64/pifdata.bin  ~/Projects/TransferBoy/transferboy64.z64
+#~/Projects/tools/cen64/cen64 -multithread -controller num=1,tpak_rom=/home/joeldipops/Projects/tools/cen64/mbc5.gbc,tpak_save=/home/joeldipops/Projects/tools/cen64/mbc5.sav ~/Projects/tools/cen64/pifdata.bin  ~/Projects/TransferBoy/transferboy64.z64
 
 # HuC Test ROM
 #~/Projects/tools/cen64/cen64 -multithread -controller num=1,tpak_rom=/home/joeldipops/Projects/tools/cen64/huc.gbc ~/Projects/tools/cen64/pifdata.bin  ~/Projects/TransferBoy/transferboy64.z64
@@ -35,3 +35,5 @@ make all
 # 2-Player Test ROM
 #~/Projects/tools/cen64/cen64 -multithread -controller num=1,tpak_rom=/home/joeldipops/Projects/tools/cen64/2player.gb ~/Projects/tools/cen64/pifdata.bin  ~/Projects/TransferBoy/transferboy64.z64
 
+# 2MB Test ROM
+~/Projects/tools/cen64/cen64 -multithread -controller num=1,tpak_rom=/home/joeldipops/Projects/tools/cen64/mbc5_2mb.gbc ~/Projects/tools/cen64/pifdata.bin  ~/Projects/TransferBoy/transferboy64.z64
