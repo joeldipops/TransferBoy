16f36433a5ceeb91321353bd607999c1a1958e50
joeldipops <joeldipops@gmail.com>
2019-06-16 20:47:48 +1000

    removed unused code

--- include/gbC/cpu.h
+++ include/gbC/cpu.h
@@ -3,9 +3,6 @@
 
 #include "types.h"
 
-// TODO - remove to gbz80ops.
-void cpu_do_cb_instruction(GbState* s);
-
 void cpu_init_emu_cpu_state(GbState *s);
 void cpu_reset_state(GbState *s);
 void cpu_step(GbState *s);
